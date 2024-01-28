9a33fb7299424b842cb181fd030d580144b99c6b
Mohammad Sameer <155916546+MDS-Sameer012@users.noreply.github.com>
2024-01-28 10:43:03 +0000

    bisection practice 28 jan 2024

--- PYQs/Bisection Method/practice_bisection_method.cpp
+++ PYQs/Bisection Method/practice_bisection_method.cpp
@@ -4,51 +4,62 @@
 
 using namespace std;
 
-double equation(double x){
-    return (pow(x,3) - (4*x) -9);
+double equation(double x)
+{
+    return ((pow(x, 2)) + (2 * x) - 3);
 }
 
-double bisectionMethod(double &low , double &high ,double tolerance , int &iteration){
-    
-    // checking if given interval is valid or not 
-    if(equation(low)*equation(high)>0){
-        cerr<<"Invalid Interval , The function should give opposite sign for give interval endpoints"<<endl;
+double bisectionMethod(double &low, double &high, double tolerance, int &iteration)
+{
+    // checking if given interval is correct
+    if (equation(low) * equation(high) > 0)
+    {
+        cerr << "Invalid Interval , The endpoint of interval should return opposite sign for f(x)" << endl;
         return NAN;
     }
-    
+
     double x;
-    cout<<"iteration \t low \t high \t x \t f(x)";
-    do{
-        x = (low+high)/2;
+
+    do
+    {
         iteration++;
-        cout<<iteration<<" \t "<<low<<" \t "<<high<<" \t "<<x<<" \t "<<equation(x)<<endl;
-        if((equation(x)==0) || (fabs(high-low)<tolerance))
+        x = (low + high) / 2;
+
+        printf("Iteration : %d \t Low : %.2f \t High : %.2f \t x : %.2f \t f(x) : %.2f \n", iteration, low, high, x, equation(x));
+
+        if ((equation(x) == 0) || (fabs(high - low) < tolerance))
         {
             return x;
         }
-        else if(equation(x)>0){
+        else if (equation(x) < 0)
+        {
+            low = x;
+        }
+        else
+        {
             high = x;
         }
-        else{
-            low = x; 
-        }
-    }while(true);
-    // return x;
+    } while (true);
 }
 
-int main(){
-    double low = 2.0;
-    double high = 3.0;
-    double tolerance = 0.001;
+int main()
+{
+
+    double low = 0;
+    double high = 3;
+    double tolerance = 0.01;
     int iteration = 0;
 
-    double root  = bisectionMethod(low,high,tolerance,iteration);
+    double root = bisectionMethod(low, high, tolerance, iteration);
 
-    cout<<endl<<"Final Values "<<endl<<endl;
-    cout<<"Iterations : "<<fixed<<setprecision(3)<<iteration<<endl;
-    cout<<"Low : "<<fixed<<setprecision(3)<<low<<endl;
-    cout<<"High : "<<fixed<<setprecision(3)<<high<<endl;
-    cout<<"f(x) : "<<fixed<<setprecision(3)<<equation(root)<<endl;
-    cout<<"x : "<<fixed<<setprecision(3)<<root<<endl;
+    cout << "*******************************************" << endl
+         << endl;
+    cout << "FINAL RESULT" << endl;
+    cout << endl
+         << "Number of Iteration : " << iteration << endl;
+    cout << "low : " <<fixed<<setprecision(2)<< low << endl;
+    cout << "high : " <<fixed<<setprecision(2)<< high << endl;
+    cout << "x(root) : " <<fixed<<setprecision(2)<< root << endl;
+
     return 0;
 }
